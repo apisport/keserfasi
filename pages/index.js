2087eab281e625e5fa421300ac02b97502409089
apisport <api.sport.team@gmail.com>
2022-11-11 20:50:01 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -78,35 +78,7 @@ export default function Home() {
           </div>
         </section>{/* End Hero */}
         <main id="main">
-          {/* ======= About Section ======= */}
-          <section id="about" className="about">
-            <div className="container" data-aos="fade-up">
-              <div className="row">
-                <div className="col-lg-6 order-1 order-lg-2"   >
-                  <div className="about-img">
-                    <img src="assets/img/about.jpg" alt />
-                  </div>
-                </div>
-                <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1" >
-                  <h3 style={{ color: "white" }}>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-                  <p className="fst-italic tex">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua.
-                  </p>
-                  <ul>
-                    <li><i className="bi bi-check-circle" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                    <li><i className="bi bi-check-circle" /> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                    <li><i className="bi bi-check-circle" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-                  </ul>
-                  <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum
-                  </p>
-                </div>
-              </div>
-            </div>
-          </section>{/* End About Section */}
+
           {/* ======= Why Us Section ======= */}
           <section id="why-us" className="why-us">
             <div className="container" data-aos="fade-up">
