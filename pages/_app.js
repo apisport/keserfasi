c354e7cb2ca2a0c04f6332c17a77cb737fc4242b
apisport <api.sport.team@gmail.com>
2022-11-11 19:59:54 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import '../styles/style.css'
+import '../styles/animate.css/animate.min.css'
+// import '../styles/aos/aos.css'
+import '../styles/bootstrap/css/bootstrap.min.css'
+import '../styles/bootstrap-icons/bootstrap-icons.css'
+import '../styles/boxicons/css/boxicons.min.css'
+import '../styles/glightbox/css/glightbox.min.css'
+import '../styles/swiper/swiper-bundle.min.css'
+import '../styles/swiper/swiper-bundle.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
