a1458ef9f4f2cf9565b314844a65e6afb782d1e8
apisport <api.sport.team@gmail.com>
2022-12-19 13:34:31 +0700

    pussss

--- pages/admin/data-pesanan.js
+++ pages/admin/data-pesanan.js
@@ -7,33 +7,38 @@ export default function Datapesanan() {
                 {/* ======= Hero Section ======= */}
                 <section id="hero" className="d-flex align-items-center">
                     <div className="container" >
-                        <div className="row text-white p-3">
-                            <table className="text-white table ">
+                        <div className=" text-white p-3">
+                            <table className="text-white table text-center">
                                 <thead>
-                                    <tr>
+                                    <tr className="bg-dark">
+                                        <th>No</th>
                                         <th>Nama</th>
-                                        <th>Tgl boking</th>
-                                        <th>NO.Hp</th>
+                                        <th>Ruangan</th>
+                                        <th>Jam</th>
+                                        <th>Paket</th>
+                                        <th>Pax</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody style={{ height: '10px !important', overflow: 'scroll' }}>
                                     <tr>
-                                        <td className="filterable-cell">Nama</td>
-                                        <td className="filterable-cell">08124</td>
-                                        <td className="filterable-cell">12425</td>
+                                            <td className="filterable-cell">1</td>
+                                            <td className="filterable-cell">orang</td>
+                                            <td className="filterable-cell">Rumah adat</td>
+                                            <td className="filterable-cell">12:00</td>
+                                            <td className="filterable-cell">Paket</td>
+                                            <td className="filterable-cell">Pax</td>
                                         <td className="filterable-cell">
                                             <div className="row ">
                                                 <div className="col-4">
-                                                    <a className="btn d-block btn-success text-white ">Terima</a>
+                                                        <a className="btn d-block btn-success text-white ">detail</a>
                                                 </div>
                                                 <div className="col-4">
-                                                    <a className="btn d-block btn-danger text-white ">Tolak</a>
+                                                        <a className="btn d-block btn-danger text-white ">edit</a>
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
-                                    
                                 </tbody>
                             </table>
 
