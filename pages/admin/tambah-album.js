d1359f2842ddd2118e10eb75564d335dc2256ad8
apisport <api.sport.team@gmail.com>
2022-12-20 16:57:01 +0700

    pusttsda

--- pages/admin/tambah-album.js
+++ pages/admin/tambah-album.js
@@ -10,7 +10,7 @@ export default function Album() {
                     </div>
                 </div>
                 <div className="container-fluid" data-aos="fade-up" data-aos-delay={100}>
-                    <div className="row g-0">
+                    <div className="row col-lg-12">
                         <div className="col-lg-3 col-md-4">
                             <div className="gallery-item">
                                 <a href="../../1.jpg" className="gallery-lightbox" data-gall="gallery-item">
@@ -77,17 +77,17 @@ export default function Album() {
                         <p>Lengkapi Data Reserfasi</p>
                     </div>
                     <form action="forms/book-a-table.php" method="post" role="form" className="php-email-form" data-aos="fade-up" data-aos-delay={100}>
-                        <div className="row col-6">
-                            <div className="col-lg-10 col-md-10 form-group mt-3">
+                        <div className="row col-lg-12">
+                            <div className="col-lg-6 col-md-10 form-group mt-3">
                                 <label style={{ color: "white" }}>Gambar</label>
                                 <input type="file" className="form-control" placeholder="Date" />
                             </div>
-                            <div className="col-lg-10 col-md-10 mt-3 form-group">
+                            <div className="col-lg-6 col-md-10 mt-3 form-group">
                                 <label style={{ color: "white" }}>Nama Ruangan</label>
                                 <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                 <div className="validate" />
                             </div>
-                            <div className="col-lg-10 col-md-10 form-group mt-3">
+                            <div className="col-lg-6 col-md-10 form-group mt-3">
                                 <label style={{ color: "white" }}>Deskripsi</label>
                                 <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                                 <div className="validate" />
--- pages/admin/tambah-paket.js
+++ pages/admin/tambah-paket.js
@@ -35,17 +35,17 @@ export default function Tambahpaket() {
                         <p>Lengkapi Data Reserfasi</p>
                     </div>
                     <form action="forms/book-a-table.php" method="post" role="form" className="php-email-form" data-aos="fade-up" data-aos-delay={100}>
-                        <div className="row col-6">
-                            <div className="col-lg-10 col-md-10 form-group mt-3">
+                        <div className="row col-lg-12">
+                            <div className="col-lg-6 col-md-10 form-group mt-3">
                                 <label style={{ color: "white" }}>Gambar</label>
                                 <input type="file" className="form-control" placeholder="Date" />
                             </div>
-                            <div className="col-lg-10 col-md-10 mt-3 form-group">
+                            <div className="col-lg-6 col-md-10 mt-3 form-group">
                                 <label style={{ color: "white" }}>Nama Ruangan</label>
                                 <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                                 <div className="validate" />
                             </div>
-                            <div className="col-lg-10 col-md-10 form-group mt-3">
+                            <div className="col-lg-6 col-md-10 form-group mt-3">
                                 <label style={{ color: "white" }}>Deskripsi</label>
                                 <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                                 <div className="validate" />
