4fcdcd6f452713b1814210a84b3e728d2b5468ee
apisport <api.sport.team@gmail.com>
2022-12-21 13:17:13 +0700

    pusttsda

--- pages/admin/tambah-ruang.js
+++ pages/admin/tambah-ruang.js
@@ -8,7 +8,6 @@ export default function Tambahruang() {
                 </div>
                 <div className="row text-white">
                     <div className="col-lg-4">
-                        <a style={{ textDecoration: 'none', color: 'white' }} href="../../pesan" >
                             <div className="gallery-item">
                                 <img src="../1.jpg" alt className="img-fluid" />
                             </div>
@@ -17,10 +16,8 @@ export default function Tambahruang() {
                                 <h4>Ruangan</h4>
                                 <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
                             </div>
-                        </a>
                     </div>
                     <div className="col-lg-4 mt-4 mt-lg-0">
-                        <a style={{ textDecoration: 'none', color: 'white' }} href="../../pesan">
                             <div className="gallery-item">
                                 <img src="../1.jpg" alt className="img-fluid" />
                             </div>
@@ -29,10 +26,8 @@ export default function Tambahruang() {
                                 <h4>Ruangan</h4>
                                 <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
                             </div>
-                        </a>
                     </div>
                     <div className="col-lg-4 mt-4 mt-lg-0">
-                        <a style={{ textDecoration: 'none', color: 'white' }} href="../../pesan">
                             <div className="gallery-item">
                                 <img src="../1.jpg" alt className="img-fluid" />
                             </div>
@@ -41,7 +36,6 @@ export default function Tambahruang() {
                                 <h4> Ruangan</h4>
                                 <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
                             </div>
-                        </a>
                     </div>
                 </div>
             </div>
